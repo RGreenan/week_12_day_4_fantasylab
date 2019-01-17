c49758d09633bd7af4caad2693c231be57dc640a
Rebekah <rebekah_7567@hotmail.co.uk>
2019-01-17 11:56:20 +0000

    set up mage and warlock files

--- src/main/java/Mage.java
+++ src/main/java/Mage.java
@@ -2,21 +2,24 @@ import java.util.ArrayList;
 
 public abstract class Mage extends Player {
 
-    private ArrayList<SpellType> spells;
+    protected ArrayList<SpellType> spells;
     private CreatureType creature;
-
 
     public Mage(double healthpoints, String name, CreatureType creature) {
         super(healthpoints, name);
         this.creature = creature;
         this.spells = new ArrayList<SpellType>();
+    }
 
-        public ArrayList<SpellType> getSpells() {
+        public ArrayList<SpellType> getSpells(){
             return spells;
+        }
+
+        public int countSpells(){
+            return spells.size();
         }
 
         public CreatureType getCreature(){
             return creature;
         }
-    }
 }
